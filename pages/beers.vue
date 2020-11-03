2a72da2ba82f41c0b8c109bb283555d9351e1f49
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2020-11-03 19:00:00 +0100

    fix: types

--- pages/beers.vue
+++ pages/beers.vue
@@ -5,7 +5,7 @@
         v-for="beerType in types"
         :key="beerType"
         class="uppercase mx-2 shadow bg-indigo-800 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded"
-        @click="FilterBeersByType(type)"
+        @click="FilterBeersByType(beerType)"
       >
         {{ beerType }}
       </button>
